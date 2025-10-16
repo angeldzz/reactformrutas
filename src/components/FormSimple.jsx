566a0bcb787aa6f38fb3a56fe10012babe82183f
angeldzz <pintodiazangel@gmail.com>
2025-10-16 09:16:14 +0200

    Imprimir por pantalla valores del formulario

--- src/components/FormSimple.jsx
+++ src/components/FormSimple.jsx
@@ -2,19 +2,24 @@ import React, { Component } from 'react'
 
 export default class FormSimple extends Component {
     cajaNombre = React.createRef();
-
+    state = {
+      nombre:""
+    }
     peticionFormulario = (event) => {
         // hay que evitar que se envie el formulario como con turi
         event.preventDefault();
-        console.log("Formulario Enviado");
         //RECUPERAR EL VALOR DE UN OBJETO REACT
         let nombre = this.cajaNombre.current.value;
         console.log("NOmbre recibido " + nombre);
+        this.setState({
+          nombre:nombre
+        })
     }
   render() {
     return (
       <div>
         <h1>Formulario simple React</h1>
+        <h2 style={{color:"red"}}>Nombre: {this.state.nombre}</h2>
         <form onSubmit={this.peticionFormulario}>
             <label>Nombre: </label>
             <input type="text" ref={this.cajaNombre} />
