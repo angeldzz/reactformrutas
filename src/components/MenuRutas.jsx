31721c60dd4547e761288260d7366b5f6c646f9e
angeldzz <pintodiazangel@gmail.com>
2025-10-16 10:57:37 +0200

    cambio lista

--- src/components/MenuRutas.jsx
+++ src/components/MenuRutas.jsx
@@ -3,15 +3,15 @@ export default class MenuRutas extends Component {
   render() {
     return (
       <div>
-        <li>
-            <ul><a href='/'>HOME</a></ul>
-            <ul><a href='/cine'>CINE</a></ul>
-            <ul><a href='/musica'>MUSICA</a></ul>
-            <ul><a href='/formsimple'>Formulario</a></ul>
-            <ul><a href='/collatz'>Collatz</a></ul>
-            <ul><a href='/multiplicar'>Tabla Multiplicar</a></ul>
-            <ul><a href='/multiplicarv2'>Tabla Multiplicar v2</a></ul>
-        </li>
+        <ul>
+            <li><a href='/'>HOME</a></li>
+            <li><a href='/cine'>CINE</a></li>
+            <li><a href='/musica'>MUSICA</a></li>
+            <li><a href='/formsimple'>Formulario</a></li>
+            <li><a href='/collatz'>Collatz</a></li>
+            <li><a href='/multiplicar'>Tabla Multiplicar</a></li>
+            <li><a href='/multiplicarv2'>Tabla Multiplicar v2</a></li>
+        </ul>
       </div>
     )
   }
