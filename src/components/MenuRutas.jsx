a48a5549d0f25cc21903ac6eaa0e51affcfc595f
angeldzz <pintodiazangel@gmail.com>
2025-10-16 11:12:37 +0200

    mejora de rutas

--- src/components/MenuRutas.jsx
+++ src/components/MenuRutas.jsx
@@ -1,17 +1,21 @@
 import React, { Component } from 'react'
+import './MenuRutas.css'
+
 export default class MenuRutas extends Component {
   render() {
     return (
       <div>
-        <ul>
-            <li><a href='/'>HOME</a></li>
-            <li><a href='/cine'>CINE</a></li>
-            <li><a href='/musica'>MUSICA</a></li>
-            <li><a href='/formsimple'>Formulario</a></li>
-            <li><a href='/collatz'>Collatz</a></li>
-            <li><a href='/multiplicar'>Tabla Multiplicar</a></li>
-            <li><a href='/multiplicarv2'>Tabla Multiplicar v2</a></li>
+      <nav className="navbar">
+        <ul className="nav-list">
+        <li className="nav-item"><a href='/' className="nav-link">HOME</a></li>
+        <li className="nav-item"><a href='/cine' className="nav-link">CINE</a></li>
+        <li className="nav-item"><a href='/musica' className="nav-link">MUSICA</a></li>
+        <li className="nav-item"><a href='/formsimple' className="nav-link">Formulario</a></li>
+        <li className="nav-item"><a href='/collatz' className="nav-link">Collatz</a></li>
+        <li className="nav-item"><a href='/multiplicar' className="nav-link">Tabla Multiplicar</a></li>
+        <li className="nav-item"><a href='/multiplicarv2' className="nav-link">Tabla Multiplicar v2</a></li>
         </ul>
+      </nav>
       </div>
     )
   }
